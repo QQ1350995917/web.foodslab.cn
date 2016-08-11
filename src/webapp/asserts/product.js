895420ec4bb5aa42ddf2f4bcf3637189941ec1d5
DingPengwei <www.dingpengwei@gmail.com>
2016-08-11 11:07:22 +0800

    hper text

--- src/webapp/asserts/product.js
+++ src/webapp/asserts/product.js
@@ -387,14 +387,16 @@ function initTypeEditorView(typeEntity) {
 
     let detailDescriptionContainer = document.createElement("div")
     detailDescriptionContainer.className = "detailDescriptionContainer";
+    initDetailEditor(detailDescriptionContainer, typeEntity);
     document.getElementById(MAIN_CONTENT_ID).appendChild(detailDescriptionContainer);
-
-    let craftsContainer = document.createElement("div")
-    craftsContainer.className = "craftsContainer";
-    document.getElementById(MAIN_CONTENT_ID).appendChild(craftsContainer);
 
     let submitContainer = document.createElement("div")
     submitContainer.className = "submitContainer";
+    submitContainer.innerHTML = "提交";
+    submitContainer.onclick = function () {
+        let text = document.createTextNode(document.getElementById("hyperEditor").innerHTML);
+        console.log(text);
+    };
     document.getElementById(MAIN_CONTENT_ID).appendChild(submitContainer);
 }
 
@@ -687,4 +689,105 @@ function createFormatDatePickerWidget(container, id) {
         maxDate: new Date('2020-12-31'),
         yearRange: [2015, 2020]
     });
+}
+
+class HyperEditorTool {
+    constructor(label, invoke, param, viewType) {
+        this.label = label;
+        this.invoke = invoke;
+        this.param = param;
+        this.viewType = viewType;
+    }
+}
+/**
+ * 产品详情,富文本编辑框
+ * @param container
+ * @param typeEntity
+ */
+function initDetailEditor(container, typeEntity) {
+    let toolsBar = document.createElement("div");
+
+    let hyperEditorTool1 = new HyperEditorTool("H1", "formatblock", "H1", "button");
+    let hyperEditorTool2 = new HyperEditorTool("H2", "formatblock", "H2", "button");
+    let hyperEditorTool3 = new HyperEditorTool("H3", "formatblock", "H3", "button");
+    let hyperEditorTool4 = new HyperEditorTool("H4", "formatblock", "H4", "button");
+    let hyperEditorTool5 = new HyperEditorTool("H5", "formatblock", "H5", "button");
+    let hyperEditorTool6 = new HyperEditorTool("H6", "formatblock", "H6", "button");
+    let hyperEditorTool7 = new HyperEditorTool("居左", "justifyleft", null, "button");
+    let hyperEditorTool8 = new HyperEditorTool("居中", "justifycenter", null, "button");
+    let hyperEditorTool9 = new HyperEditorTool("居右", "justifyright", null, "button");
+    let hyperEditorTool10 = new HyperEditorTool("左缩进", "outdent", null, "button");
+    let hyperEditorTool11 = new HyperEditorTool("右缩进", "indent", null, "button");
+    let hyperEditorTool12 = new HyperEditorTool("有序列", "insertorderedlist", null, "button");
+    let hyperEditorTool13 = new HyperEditorTool("无序列", "insertunorderedlist", null, "button");
+    let hyperEditorTool14 = new HyperEditorTool("链接", "createlink", null, "button");
+    let hyperEditorTool15 = new HyperEditorTool("图片", null, null, "button");
+    let hyperEditorTool16 = new HyperEditorTool("图片", "formatblock", null, "file");
+    let tools = new Array();
+    tools.push(hyperEditorTool1);
+    tools.push(hyperEditorTool2);
+    tools.push(hyperEditorTool3);
+    tools.push(hyperEditorTool4);
+    tools.push(hyperEditorTool5);
+    tools.push(hyperEditorTool6);
+    tools.push(hyperEditorTool7);
+    tools.push(hyperEditorTool8);
+    tools.push(hyperEditorTool9);
+    tools.push(hyperEditorTool10);
+    tools.push(hyperEditorTool11);
+    tools.push(hyperEditorTool12);
+    tools.push(hyperEditorTool13);
+    tools.push(hyperEditorTool14);
+    tools.push(hyperEditorTool15);
+    tools.push(hyperEditorTool16);
+
+    for (let index = 0; index < tools.length; index++) {
+        createHyperToolWidget(toolsBar, tools[index]);
+    }
+
+    let hyperEditor = document.createElement("div");
+    hyperEditor.className = "hyperEditor";
+    hyperEditor.id = "hyperEditor";
+    hyperEditor.contentEditable = true;
+    container.appendChild(toolsBar);
+    container.appendChild(hyperEditor);
+}
+
+function createHyperToolWidget(container, hyperEditorTool) {
+    let tool = document.createElement("input");
+    tool.type = hyperEditorTool.viewType;
+    tool.className = "hyperEditorToolButton";
+    if (tool.type == "file") {
+        tool.accept = "image/*"
+        tool.onchange = function () {
+            var file = window.URL.createObjectURL(tool.files.item(0));
+            console.log(file);
+            document.execCommand('insertImage', false, file);
+            // document.execCommand('insertHTML', false, "<img src='" + file + "'/>");
+            document.getElementById("hyperEditor").focus();
+        }
+    } else {
+        tool.value = hyperEditorTool.label;
+        tool.onclick = format;
+        tool.cmd = hyperEditorTool.invoke;
+        tool.param = hyperEditorTool.param;
+    }
+    container.appendChild(tool);
+}
+
+function format() {
+    if (this.cmd == "createlink") {
+        var sLnk = prompt('请输入网络地址', 'http:\/\/');
+        if (sLnk && sLnk != '' && sLnk != 'http://') {
+            document.execCommand(this.cmd, false, sLnk);
+        }
+    } else {
+        console.log(this.cmd + " , " + this.param);
+        document.execCommand(this.cmd, false, this.param);
+        document.getElementById("hyperEditor").focus();
+    }
+}
+
+function insertImage() {
+
 }
