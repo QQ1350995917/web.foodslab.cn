8166e8a8c9143a6a6dace53d895eafd84aae5ca1
DingPengwei <www.dingpengwei@gmail.com>
2016-09-01 16:17:18 +0800

    query

--- src/frontend/asserts/query.js
+++ src/frontend/asserts/query.js
@@ -14,7 +14,14 @@ window.onload = function () {
 };
 
 function requestOrderByLean(orderId) {
-
+    let url = BASE_PATH + "order/retrieve?orderId=" + orderId;
+    asyncRequestByGet(url, function (data) {
+        var result = checkResponseDataFormat(data);
+        if (result) {
+            var jsonData = JSON.parse(data);
+            onRequestOrderByLeanCallback(orderId,jsonData.data);
+        }
+    }, onErrorCallback, onTimeoutCallback);
 }
 
 function requestOrderByKey(key) {
@@ -30,6 +37,36 @@ function requestRecommend() {
             createRecommendView(jsonData.data);
         }
     }, onErrorCallback, onTimeoutCallback);
+}
+
+function onRequestOrderByLeanCallback(orderId,data) {
+    let searchResultView = document.getElementById("searchResultView");
+    searchResultView.innerHTML = null;
+    if (searchResultView.clientHeight == 0){
+        let mainView = document.getElementById(MAIN);
+        mainView.style.height = mainView.clientHeight + 100 + "px";
+    }
+    if (data == undefined) {
+        searchResultView.style.height = "100px";
+        searchResultView.innerHTML = "没有查询到订单号为<span style='color: #FF0000'>" + orderId + "</span>的订单";
+    } else {
+        searchResultView.style.height = "100px";
+
+        let orderInfo = document.createElement("div");
+        orderInfo.innerHTML = "订单号: " + data.orderId + "  下单时间: " + data.createTime;
+        let receiverInfo = document.createElement("div");
+        receiverInfo.innerHTML = "收货人: " + data.name + " " + data.province + " " + data.city + " " + data.county + " **** " +  data.phone0;
+        let expressInfo = document.createElement("div");
+        expressInfo.innerHTML = "快递公司: " + data.expressName + "    " + "快递单号: " + data.expressNumber;
+        let expressCurrentInfo = document.createElement("div");
+        expressCurrentInfo.innerHTML = "快递状态：2016年1月25日 下午7:06:38  北京市|到件|到北京市【北京分拨中心】北京市|发件|北京市【BEX北京昌平区天龙二部】，正发往【北京金盏分拨中心】";
+
+
+        searchResultView.appendChild(orderInfo);
+        searchResultView.appendChild(receiverInfo);
+        searchResultView.appendChild(expressInfo);
+        searchResultView.appendChild(expressCurrentInfo);
+    }
 }
 
 function onRequestOrderByKeyCallback(key,data) {
@@ -50,16 +87,20 @@ function onRequestOrderByKeyCallback(key,data) {
 }
 
 function createMenuDownView() {
-    let searchContainer = document.createElement("div")
+    let searchContainer = document.createElement("div");
     searchContainer.className = "searchContainer";
-    let searchInput = document.createElement("input")
+    let searchInput = document.createElement("input");
     searchInput.className = "searchInput";
     searchInput.type = "text";
-    let searchButton = document.createElement("div")
+    let searchButton = document.createElement("div");
     searchButton.className = "searchButton";
     searchButton.innerHTML = "查询订单";
     searchButton.onclick = function () {
-        requestOrderByKey(searchInput.value);
+        if (searchInput.value != null && searchInput.value != ""){
+            requestOrderByKey(searchInput.value);
+        }else{
+            new Toast().show("请输入要查询的信息");
+        }
     };
     searchContainer.appendChild(searchInput);
     searchContainer.appendChild(searchButton);
