227fc55083c16207a5b72d1e5e8590db72e80683
DingPengwei <www.dingpengwei@gmail.com>
2016-09-08 21:55:17 +0800

    stash

--- src/frontend/asserts/mine_order.js
+++ src/frontend/asserts/mine_order.js
@@ -4,4 +4,12 @@
 function requestOrder(accountId) {
     let mainView = document.getElementById(MAIN);
     mainView.innerHTML = null;
+    let url = BASE_PATH + "order/retrieve?accountId=" + accountId;
+    asyncRequestByGet(url, function (data) {
+        var result = checkResponseDataFormat(data);
+        if (result) {
+            var jsonData = JSON.parse(data);
+            console.log(jsonData);
+        }
+    }, onErrorCallback, onTimeoutCallback);
 }
